2550e69a545370b554b8c05cab40190e34322589
Diego <alfiandrodiego@gmail.com>
2022-09-27 21:10:20 +0700

    menambahkan file ke github

---
+++ script.js
@@ -0,0 +1,18 @@
+// function sum (a =4, b) {
+//     return a + b
+// }
+
+// num1 = 5
+// num2 = 10
+
+// console.log(sum(num1, num2));\
+
+// const str1 = 'diego alfiandro';
+
+// console.log(str1.toUpperCase());
+
+// const str1 = 'Hello';
+// const str2 = 'World';
+
+// console.log(str1.concat(' ', str2));
+
